9ad61c29c18b2128cbfd338c4516995aebbd9f8c
Tushar Jadhav <tusharjadhav@Tushars-Mac-mini.local>
2025-10-17 18:32:22 +0530

    Update XML file

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -42,7 +42,7 @@ public class BaseTest {
         System.out.println("Running tests on environment: " + environment);
 
         playwright = Playwright.create();
-        browser = playwright.firefox().launch(
+        browser = playwright.chromium().launch(
                 new BrowserType.LaunchOptions().setHeadless(false));
 
         BrowserContext context;
